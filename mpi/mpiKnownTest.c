11a91567fec4bbb25a0b5b9b23e300d6d2de7f90
Srinath Vadlamani <srinath@txcorp.com>
2018-07-23 09:46:55 -0700

    added a node name identifier

--- mpi/mpiKnownTest.c
+++ mpi/mpiKnownTest.c
@@ -21,6 +21,11 @@ int main(int argc, char** argv) {
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+  // Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
   // We are assuming at least 2 processes for this task
   if (world_size < 2) {
     fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
@@ -56,7 +61,8 @@ MPI_Barrier(MPI_COMM_WORLD); // sync all
     MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
   } else if (world_rank == 1) {
     MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    printf("Process 1 received number %d from process 0\n", number);
+    //printf("Process 1 received number %d from process 0\n", number);
+    printf("Process named %s with rank %d received number %d from process 0\n", processor_name, world_rank, number);
   }
 
 // rank 0 ~ 0sec in mpi but 1 rank in 10s in mpi_recv
